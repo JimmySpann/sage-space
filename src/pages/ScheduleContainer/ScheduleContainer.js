80bf24aaf93b9e2bf62e3d9f0037f32c1d2aff9c
jimmyspann <jimmyleespann@outlook.com>
2021-06-07 21:51:22 -0400

    Switch component from class to function Add EditEventDialog with button demo

--- src/pages/ScheduleContainer/ScheduleContainer.js
+++ src/pages/ScheduleContainer/ScheduleContainer.js
@@ -1,119 +1,49 @@
-import React from 'react'
-import {Event, EventModel} from '../../models/event';
+import React from 'react';
+import { useEffect, useState } from 'react'
+import { EventModel } from '../../models/event';
 import FullCalendar, { formatDate } from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import { INITIAL_EVENTS, createEventId } from './event-utils'
+import { createEventId } from './event-utils'
 import debugLog from '../../utils/customDebugging';
+import EditEventDialog from './EditEventDialog'
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-export default class ScheduleContainer extends React.Component {
+export default function ScheduleContainer() {
 
-    state = {
-      weekendsVisible: true,
-      displayedEvents: []
-    }
+  const [weekendsVisible, setWeekendsVisible] = useState(true);
+  const [displayedEvents, setDisplayedEvents] = useState([]);
 
-  
+  const [open, setOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState("Toes");
 
-  componentDidMount() {
-    // NoteModel.getAllNotes()
-    //   .then((result) => {
-    //     if(this.props.match.params.id) {
-    //       this.setState({notes: result, note: result.find(element => element._id === this.props.match.params.id)});
-    //     } else {
-    //       const recentNote = result.reduce((a, b) => (a.updatedAt > b.updatedAt ? a : b));
-    //       this.setState({notes: result, note: recentNote});
-    //       this.props.history.push(`/notes/${recentNote._id}`);
-    //     }
-    //   })
-    //   .catch((err) => console.log(err))
-    
-    let allEvents = [];
+  //ComponentDidMount
+  useEffect(() => {
     EventModel.getAll()
-      .then((result) => {
-        allEvents = result;
-        this.setState({ displayedEvents: allEvents })
-      })
-      .catch((error) => {
-        debugLog(error)
-      })
+      .then(result => setDisplayedEvents(result))
+      .catch(error => debugLog(error))
+  }, [])
+
+
+  //Dialog Controls
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+    setSelectedValue(value);
+  };
+
+
+  //Calendar Controls
+  const handleWeekendsToggle = () => {
+    setWeekendsVisible(!weekendsVisible);
   }
 
-  //Rendering
-
-  render() {
-    return (
-      <div className='demo-app'>
-        {this.renderSidebar()}
-        <div className='demo-app-main'>
-          <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            headerToolbar={{
-              left: 'prev,next today',
-              center: 'title',
-              right: 'dayGridMonth,timeGridWeek,timeGridDay'
-            }}
-            initialView='dayGridMonth'
-            editable={true}
-            selectable={true}
-            selectMirror={true}
-            dayMaxEvents={true}
-            weekends={this.state.weekendsVisible}
-            events={this.state.displayedEvents}
-            select={this.handleDateSelect}
-            eventContent={renderEventContent} // custom render function
-            eventClick={this.handleEventClick}
-            // eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-            // you can update a remote database when these fire:
-            // eventAdd={function(){}}
-            // eventChange={function(){}}
-            // eventRemove={function(){}}
-             
-          />
-        </div>
-      </div>
-    )
-  }
-
-  renderSidebar() {
-    return (
-      <div className='demo-app-sidebar'>
-        <div className='demo-app-sidebar-section'>
-          <h2>Instructions</h2>
-          <ul>
-            <li>Select dates and you will be prompted to create a new event</li>
-            <li>Drag, drop, and resize events</li>
-            <li>Click an event to delete it</li>
-          </ul>
-        </div>
-        <div className='demo-app-sidebar-section'>
-          <label>
-            <input
-              type='checkbox'
-              checked={this.state.weekendsVisible}
-              onChange={this.handleWeekendsToggle}
-            ></input>
-            toggle weekends
-          </label>
-        </div>
-        <div className='demo-app-sidebar-section'>
-          <h2>All Events ({this.state.displayedEvents.length})</h2>
-          <ul>
-            {/* {this.state.displayedEvents.map(renderSidebarEvent)} */}
-          </ul>
-        </div>
-      </div>
-    )
-  }
-
-  handleWeekendsToggle = () => {
-    this.setState({
-      weekendsVisible: !this.state.weekendsVisible
-    })
-  }
-
-  handleDateSelect = (selectInfo) => {
+  const handleDateSelect = (selectInfo) => {
     let title = prompt('Please enter a new title for your event')
     let calendarApi = selectInfo.view.calendar
 
@@ -132,6 +62,7 @@ export default class ScheduleContainer extends React.Component {
 
       EventModel.create(event)
        .then((newEvent) => {
+         debugLog("RES",newEvent)
          calendarApi.addEvent(newEvent)
        })
        .catch(error => {
@@ -141,19 +72,13 @@ export default class ScheduleContainer extends React.Component {
     }
   }
 
-  handleEventClick = (clickInfo) => {
-
-    debugLog(clickInfo.event)
-
+  const handleEventClick = (clickInfo) => {
     if (window.confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      
-      EventModel.delete(clickInfo.event)
-      .then((newEvent) => {
+      const id = clickInfo.event.extendedProps._id
+      EventModel.delete({_id: id})
+      .then((deletedEvent) => {
         clickInfo.event.remove()
-        let currentEvents = this.state.displayedEvents
-        this.setState({
-          displayedEvents: [...currentEvents, newEvent]
-        })
+        setDisplayedEvents(displayedEvents.filter(event => event._id !== deletedEvent._id)) //Got to be a better way. Fix later
       })
       .catch(error => {
        console.log("error", error)
@@ -162,28 +87,104 @@ export default class ScheduleContainer extends React.Component {
     }
   }
 
+  useEffect(() => {
+    debugLog("displayedEvents", displayedEvents)
+  }, [displayedEvents])
+
   // handleEvents = (events) => {
   //   this.setState({
   //     displayedEvents: events
   //   })
   // }
 
-}
+  function renderSidebar() {
+    return (
+      <div className='demo-app-sidebar'>
+        <div className='demo-app-sidebar-section'>
+          <h2>Instructions</h2>
+          <ul>
+            <li>Select dates and you will be prompted to create a new event</li>
+            <li>Drag, drop, and resize events</li>
+            <li>Click an event to delete it</li>
+          </ul>
+        </div>
+        <div className='demo-app-sidebar-section'>
+          <label>
+            <input
+              type='checkbox'
+              checked={weekendsVisible}
+              onChange={handleWeekendsToggle}
+            ></input>
+            toggle weekends
+          </label>
+        </div>
+        <div className='demo-app-sidebar-section'>
+          <h2>All Events ({displayedEvents.length})</h2>
+          <ul>
+            {/* {this.state.displayedEvents.map(renderSidebarEvent)} */}
+          </ul>
+        </div>
+      </div>
+    )
+  }
 
-function renderEventContent(eventInfo) {
+  function renderEventContent(eventInfo) {
   return (
     <>
       <b>{eventInfo.timeText}</b>
       <i>{eventInfo.event.title}</i>
     </>
   )
-}
+  }
 
-function renderSidebarEvent(event) {
+  function renderSidebarEvent(event) {
   return (
     <li key={event._id}>
       <b>{formatDate(event.start, {year: 'numeric', month: 'short', day: 'numeric'})}</b>
       <i>{event.title}</i>
     </li>
   )
+  }
+
+  //Rendering
+  return (
+    <div className='demo-app'>
+      {renderSidebar()}
+      <div className='demo-app-main'>
+        <div>
+          <Typography variant="subtitle1">Selected: {selectedValue}</Typography>
+          <br />
+          <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+            Open simple dialog
+          </Button>
+          <EditEventDialog selectedValue={selectedValue} open={open} onClose={handleClose} />
+        </div>
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          }}
+          initialView='dayGridMonth'
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          weekends={weekendsVisible}
+          events={displayedEvents}
+          select={handleDateSelect}
+          eventContent={renderEventContent} // custom render function
+          eventClick={handleEventClick}
+          // eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+          // you can update a remote database when these fire:
+          // eventAdd={function(){}}
+          // eventChange={function(){}}
+          // eventRemove={function(){}}
+           
+        />
+      </div>
+    </div>
+  )
+
 }
