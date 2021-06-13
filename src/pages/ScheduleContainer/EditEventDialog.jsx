e6012fbcd209ab7ad838a9ef447010c95748319f
jimmyspann <jimmyleespann@outlook.com>
2021-06-13 15:53:51 -0400

    add custom date picker

--- src/pages/ScheduleContainer/EditEventDialog.jsx
+++ src/pages/ScheduleContainer/EditEventDialog.jsx
@@ -14,6 +14,18 @@ import AddIcon from '@material-ui/icons/Add';
 import Typography from '@material-ui/core/Typography';
 import { blue } from '@material-ui/core/colors';
 
+import "./ScheduleContainer.css"
+
+import 'date-fns';
+// import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
 const emails = ['username@gmail.com', 'user02@gmail.com'];
 const useStyles = makeStyles({
   avatar: {
@@ -34,11 +46,17 @@ function SimpleDialog(props) {
     onClose(value);
   };
 
+
+  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
   return (
-    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
-      <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
-      <List>
-        {emails.map((email) => (
+    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open} className="dialog">
+      <DialogTitle id="simple-dialog-title">Edit Event</DialogTitle>
+        {/* {emails.map((email) => (
           <ListItem button onClick={() => handleListItemClick(email)} key={email}>
             <ListItemAvatar>
               <Avatar className={classes.avatar}>
@@ -47,20 +65,77 @@ function SimpleDialog(props) {
             </ListItemAvatar>
             <ListItemText primary={email} />
           </ListItem>
-        ))}
+        ))} */}
 
-        <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
+        {/* <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
           <ListItemAvatar>
             <Avatar>
               <AddIcon />
             </Avatar>
           </ListItemAvatar>
           <ListItemText primary="Add account" />
-        </ListItem>
-      </List>
+        </ListItem> */}
+
+
+
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <KeyboardDatePicker
+                      disableToolbar
+                      variant="inline"
+                      format="MM/dd/yyyy"
+                      margin="normal"
+                      id="date-picker-inline"
+                      label="Date picker inline"
+                      value={selectedDate}
+                      onChange={handleDateChange}
+                      KeyboardButtonProps={{
+                        'aria-label': 'change date',
+                      }}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    <KeyboardDatePicker
+                      margin="normal"
+                      id="date-picker-dialog"
+                      label="Date picker dialog"
+                      format="MM/dd/yyyy"
+                      value={selectedDate}
+                      onChange={handleDateChange}
+                      KeyboardButtonProps={{
+                        'aria-label': 'change date',
+                      }}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    <KeyboardTimePicker
+                      margin="normal"
+                      id="time-picker"
+                      label="Time picker"
+                      value={selectedDate}
+                      onChange={handleDateChange}
+                      KeyboardButtonProps={{
+                        'aria-label': 'change time',
+                      }}
+                    />
+                </Grid>
+            </Grid>
+        </MuiPickersUtilsProvider>
     </Dialog>
   );
 }
+
+// export default function MaterialUIPickers() {
+//   // The first commit of Material-UI
+
+
+//   return (
+
+//   );
+// }
 
 SimpleDialog.propTypes = {
   onClose: PropTypes.func.isRequired,
